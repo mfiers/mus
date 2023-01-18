562d247eb2fb694ba6bfa8a2567e45e1e0eefa6a
Mark Fiers <mark.fiers.42@gmail.com>
2023-01-18 09:25:10 +0100

    checksums for linux

--- mus/util/files.py
+++ mus/util/files.py
@@ -11,7 +11,12 @@ def get_checksum(filename: Path) -> str:
             ['shasum', '-U', '-a', '256', filename],
             text=True)
         checksum = P.split()[0]
+    elif sys.platform == 'linux':
+        P = sp.check_output(
+            ['shasum', '-p', '-a', '256', filename],
+            text=True)
+        checksum = P.split()[0]
     else:
-        raise NotImplementedError()
+        raise NotImplementedError(f"for sys {sys.platform}")
 
     return checksum
