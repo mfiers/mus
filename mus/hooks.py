905fd7d62426ed7e102b6fee52b70ed71c7540f3
Mark Fiers <mark.fiers.42@gmail.com>
2023-04-16 09:44:39 +0200

    docs, ensure high priority values go first

--- mus/hooks.py
+++ mus/hooks.py
@@ -16,6 +16,14 @@ HOOKS: dict = defaultdict(list)
 def register_hook(name: str,
                   func,
                   priority: int = 10):
+    """
+    Register a hook to be called later
+
+    Args:
+        name (str): Name of the hook to associate the function with
+        func (callable): function to call
+        priority (int, optional): Priority - higher goes first. Defaults to 10.
+    """
     HOOKS[name].append(
         (priority, func))
 
@@ -29,8 +37,8 @@ def call_hook(name: str,
         name (str): Name of the hook.
         kargs: remainer of arguments passed to the hook.
     """
-    for priority, func in sorted(HOOKS.get(name, []), key=lambda x: x[0]):
+    for priority, func in sorted(HOOKS.get(name, []), key=lambda x: -x[0]):
 
-        lg.debug(
+        lg.warning(
             f"Call hook {func.__name__} with priority {priority}")
         func(**kwargs)
