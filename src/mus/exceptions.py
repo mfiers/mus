56bc5cc7629efcbd1602404d8ddf77dc30d42a58
Mark Fiers <mark.fiers.42@gmail.com>
2024-11-16 08:48:57 +0100

    moved to eln plugin

--- src/mus/exceptions.py
+++ src/mus/exceptions.py
@@ -1,19 +1,3 @@
 
 class InvalidConfigFileEntry(Exception):
     pass
-
-
-class ElnApiKeyNotDefined(Exception):
-    pass
-
-
-class ElnURLNotDefined(Exception):
-    pass
-
-
-class ElnNoExperimentId(Exception):
-    pass
-
-
-class ElnConflictingExperimentId(Exception):
-    pass
